ff2cc24d8ce9914e247eba123b9b013f5c856711
Skorikana <sandy.pramoda90@gmail.com>
2023-03-28 14:15:44 -0400

    first

---
+++ script.js
@@ -0,0 +1,6 @@
+//The game is between a a computer and a player.The screen displays the jumbled world
+// and  the player should guess the correct word.
+//If the user is able to type the correct word the message should be displayed as "Dat was correct"
+//If the user did not attempt correctly it should display(may be  a window alert)..."Good try but u missed..try again"
+//It should two have 2 buttons 1.submit 2.reset
+//If the functionality works then would go with other types likes presidents or any other famous places
