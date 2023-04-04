e516873d04abdbfac42b1708ee6c65441931d0d2
Skorikana <sandy.pramoda90@gmail.com>
2023-04-04 09:46:16 -0400

    fifthcommit

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
 
 const easycountry =[ "FRANCE","ROME","PERU","UNITEDSTATES" ,"GREECE","INDIA","EGYPT", "CHINA" ,"CUBA","JORDAN","ENGLAND","MEXICO" ,
-                     "BRAZIL","JAPAN","POLAND","CANADA","PORTUGAL","RUSSIA","MALAYSIA","THAILAND","ALASKA","SPAIN","GERMANY","UKRAINE",
-                      "ITALY" , "TURKEY", "IRELAND" ,"BAHAMAS","CHILE","NEPAL"]; 
+                     "BRAZIL","JAPAN","POLAND","CANADA","PORTUGAL","RUSSIA","MALAYSIA","THAILAND","SPAIN","GERMANY","UKRAINE",
+                      "ITALY" , "TURKEY", "IRELAND" ,"BAHAMAS","CHILE","ALASKA"]; 
 
-const mediumcountry =["NETHERLANDS", "HONGKONG","UNITEDKINGDOM" ,"CROATIA","SOMALIA","TUVALU","SUDAN","SRILANKA","NEPAL","BHUTAN","MYANMAR",
-                      "PAKISTAN","NIGERIA","BANGLADESH","SWEDEN","SWITZERLAND","SWEDEN","THAILAND","AUSTRIA","MACAU","HUNGARY","SINGAPORE",
+const mediumcountry =["NETHERLANDS","UNITEDKINGDOM" ,"CROATIA","SOMALIA","TUVALU","SUDAN","SRILANKA","NEPAL","BHUTAN","MYANMAR",
+                      "PAKISTAN","NIGERIA","BANGLADESH","SWEDEN","SWITZERLAND","THAILAND","AUSTRIA","MACAU","HUNGARY","SINGAPORE",
                       "KOREA","BELGIUM","BELARUS","ANGOLA","AZERBAIJAN","LIBYA","PHILIPPINES","CAMBODIA"];
 
 const hardcountry =["GRENADINES","LIECHTENSTEIN","DOMINICA" ,"TIMOR-LESTE","DJIBOUTI","SAO TOME","SIERRA LEONE","GINE-BISSAU",
                    "GUINEA","TONGA","MAURITANIA","COMOROS","LIBERIA","KIRIBATI","NAURU","BENIN","MALDOVA","PALAU","CZECHREPUBLIC","ECUADOR",
                   "ELSALVADOR","ERITREA","ESTONIA","KYRGYZSTAN","LATVIA","LESOTHO","MOZAMBIQUE","RWANDA","TAJIKISTAN","SURINAME"];
 
-const wordText = document.querySelector(".word");  //accessing the p class which displays the scrambled word
+const wordText = document.querySelector(".word");  //Accessing the p class which displays the scrambled word
 const selectedLevel =document.querySelector(".selectedLevel");
+const winRule  = document.querySelector(".winRule");  //Determines the rule for the user to win according to the level
 
-let currentLevel = "";            //declaring result and currentlevel as empty so dat it can be redeclared and value can be used to store userinput
-let result =""; 
-let sucess = 0;                              
+let currentLevel = "";            //Declaring result and currentlevel as empty so dat it can be redeclared and value can be used to store userinput
+let result ="";                   //whether the userinput is correct or not
 
-function game() {                 //initialising a function game and using math.randomm formula 
+let sucess = 0;                  //How many the user got correct     
+let levelSucess = 0;             // To know how many questions user need to get correct  in particular level.
+
+
+function game() {                 //initialising a function:game and using math.randomm formula 
   if(currentLevel == "EASY"){
     result = easycountry[Math.floor(Math.random() * easycountry.length)];    //looping to find the user input level so that it can display random word from that particular array
+    levelSucess = 5;              //determines how many user needs to get correct straight in a row
+  
   }
   else if(currentLevel == "MEDIUM"){
     result = mediumcountry[Math.floor(Math.random() * mediumcountry.length)];
+    levelSucess = 11;
+      
   }
   else if(currentLevel == "HARD")
   {
     result = hardcountry[Math.floor(Math.random() * hardcountry.length)];
+    levelSucess = 23;
+  
   }
   
   const scrambled = result.split('').sort(function(){return 0.3 -Math.random()}).join('');//after getting the random word(result) it would split 
@@ -39,17 +49,20 @@ function game() {                 //initialising a function game and using math.
   game();
 
 
-  function buttonClick(event){            //button click function to know exactly which level user chose and to display words from that particular array.
-    let text = event.target.innerHTML;   //event.target gives us access to the DOM element the user clicked.
-    // if(text == "EASY")  {
-      // document.getElementById("chooseLevel").style.display = "none";
-      // document.body.style.backgroundColor ="lightgreen";          
-    //}                                           //console.log(text);//console.log(currentLevel);
-     currentLevel = text;
-    selectedLevel.innerHTML = "The selected Level is " + currentLevel;             //console.log(currentLevel);
-    
-
+  function buttonClick(event){                  //button click function to know exactly which level user chose and to display words from that particular array.
+    let text = event.target.innerHTML;          //event.target gives us access to the DOM element the user clicked.
+    currentLevel = text;                        //console.log(text);//console.log(currentLevel);
+    if(currentLevel == "EASY"){                         
+      winRule.innerHTML = "Get 6 right to Win";
+    }
+    else if(currentLevel == "MEDIUM"){
+      winRule.innerHTML = "Get 12 right to Win";
+    }
+    else if(currentLevel == "HARD"){
+      winRule.innerHTML = "Get 24 right to Win";
+    }
   }
+
 
   function submit(){
   let userinput = document.getElementById("userinput").value; //To know what the user has given in the textbox when user clicked on the submit button
@@ -57,73 +70,89 @@ function game() {                 //initialising a function game and using math.
     if(userinput== ''){                 
      alert("Uh -Oh!! Its Empty..Please enter the word");      //if the user-input is empty it would pop up a message 
     }
-   else if(userinput.toLowerCase() == result.toLowerCase()){   //userinput would be considered even its in both lower aswell as uppercase
-     sucess = sucess+1;
-    if(sucess >6){
-      alert("you win");
-      sucess = 0 ;
-    } 
-    else{
-    alert("BINGO!!...Time for the Next One");
-    }
-    game();
-   }
-   else {
-    sucess=0;
-    alert("Hmm.. Looks like "+ userinput.toUpperCase() + "  is Wrong...Try Again!!");      //highlighted in uppercase so dat user can identify what exactly he typed. 
+    else if(userinput.toLowerCase() == result.toLowerCase()){   //userinput would be considered even its in both lower aswell as uppercase
+            sucess = sucess+1;
+            if(sucess >levelSucess){
+            alert("Congratulationsss!!!You Won!!!!"  );                     
+            sucess = 0 ;
+            } 
+            else{
+            alert("BINGO!!...You got it Right!!!Time for Next One!!." + "Your score is "+ sucess);
+            }
+           game();
+            }
+   else {                                               
+    sucess=0;       //the score board sets back to 0  once the user answers 1 question wrong and game starts again.           
+    alert("Hmm.. Looks like "+ userinput.toUpperCase() + "  is Wrong...Try Again!!");   //highlighted in uppercase so dat user can identify what exactly he typed. 
    }
   
    document.getElementById("userinput").value = "";      // to clear the user input textbox after comparisons/validations
   }
 
-  
-    const easyBtn = document.getElementById("easybutton");         //accessing the easy level button..
-    easyBtn.addEventListener("click" ,buttonClick);                //calling buttonClick func to know which level user has selected
-    easyBtn.addEventListener("click" ,function onClick(event){     //turning the background color 
-    document.getElementById("chooseLevel").style.display = "none"; //once the user has choosen level..the h2 tag display is hidden to display the scrambled word
-    document.body.style.backgroundColor ="lightgreen";});          //changing the background color to green                                                 
-    
 
-   const mediumBtn = document.getElementById("mediumbutton");
+   //accessing the  level buttons..
+
+    const easyBtn = document.getElementById("easybutton");        
+    const mediumBtn = document.getElementById("mediumbutton");
+    const hardBtn = document.querySelector(".hard");
+
+   //accessing background image as it needs to be changed for all the three levels 
+    const bgImg = document.querySelector("background");
+
+    
+    easyBtn.addEventListener("click" ,buttonClick);                //calling buttonClick func to know which level user has selected
+    easyBtn.addEventListener("click" ,function onClick(event){     
+    document.getElementById("chooseLevel").style.display = "none"; //once the user has choosen level..the h2 tag display is hidden to display the scrambled word
+      mediumBtn.disabled = true;  
+      hardBtn.disabled =true;
+    document.body.style.backgroundImage ="url\('easy.jpg')"});
+                       
+
    mediumBtn.addEventListener("click" ,buttonClick);
    mediumBtn.addEventListener("click" ,function onClick(event){
+    easyBtn.disabled = true;
+    hardBtn.disabled = true;
    document.getElementById("chooseLevel").style.display = "none";
-   document.body.style.backgroundColor ="purple"});
-  
+   document.body.style.backgroundImage ="url\('medium.jpg')"});
 
-   
-    const hardBtn = document.querySelector(".hard");
+
     hardBtn.addEventListener("click" ,buttonClick);
     hardBtn.addEventListener("click",function onClick(event){
+      easyBtn.disabled=true;
+      mediumBtn.disabled =true;
     document.getElementById("chooseLevel").style.display = "none";
-    document.body.style.backgroundColor ="red" });
-
+    document.body.style.backgroundImage ="url\('hard.png')"});
+      
+  //Added eventlistener to 3 levels so that once userinputs their desired level the game function would run and words would be displayed
    
-    easyBtn.addEventListener("click" ,game);             //added eventlistener to 3 levels so that once userinputs their desired level
-    mediumBtn.addEventListener("click" ,game);           //the game function would run and words would be displayed
+    easyBtn.addEventListener("click" ,game);             
+    mediumBtn.addEventListener("click" ,game);           
     hardBtn.addEventListener("click" ,game);
 
-  
-   const submitBtn =document.querySelector(".Submit");    //accessing the submit button and added eventlistener click so dat submit function is called
-   submitBtn.addEventListener("click" ,submit);           //to validate the userinput
+   //accessing the submit button and added eventlistener click so dat submit function is called to validate the userinput
+
+   const submitBtn =document.querySelector(".Submit");   
+   submitBtn.addEventListener("click" ,submit);           
    
-   const refreshBtn =document.querySelector(".Refresh");  //accessing the refresh button to change the scrambled word 
+  //Accessing the refresh button to change the scrambled word,score/sucess comes off to zero
+
+   const refreshBtn =document.querySelector(".Refresh");   
    refreshBtn.addEventListener("click" ,game);
- 
-   let resetBtn = document.querySelector(".Reset")                     //accessing the reset button and added a function so that when user clicks the button
-   resetBtn.addEventListener("click" ,function onClick(event){         //the page and game would reset to start.
+   refreshBtn.addEventListener("click" ,function onClick(event){        
+   sucess = 0});
+
+  //Accessing the reset button and added a function so that when user clicks the button the page would reset
+   
+   let resetBtn = document.querySelector(".Reset")                    
+   resetBtn.addEventListener("click" ,function onClick(event){         
    document.location.reload()});
   
-//manipulating submit button as same as enter button on keyboard.The user can enter his answer and click on ENTER on keyboard insted of submit button every time   
-  const input = document.getElementById("userinput");        //accessing the answerbox with "id " and storing in a variable
+
+   //Manipulating submit button as same as enter button on keyboard.The user can enter his answer and click on ENTER on keyboard insted of submit button every time   
+   const input = document.getElementById("userinput");        //accessing the answerbox with "id " and storing in a variable
    input.addEventListener("keypress", function(event) {      //adding an event "keypress" so that when user clicks "enter" button
    if (event.key === "Enter") {
     event.preventDefault();                                  //cancelling the default action if needed..in dis case its submit button click
     document.getElementById("enter").click();                //accesing submit button id:"enter" and calling the click function so that once the user enters the answer and can click enter as submitting.
   }
 });
-
-// const bgImg = document.querySelector("background");
-// hardBtn.addEventListener("click",function onClick(event){
-// document.body.style.backgroundImage ="url\('collage3.jpg')"});
-
